ee504599f275b43132575a076ea05c69020c5dfb
Vadim <vtimofeev223@gmail.com>
2021-06-17 06:44:56 +0300

    fix manager.js

--- vue/manager.js
+++ vue/manager.js
@@ -1,18 +1,19 @@
-import settings from "../../BrandingWebclient/vue/settings";
+import settings from '../../BrandingWebclient/vue/settings'
+
 export default {
-	name: 'BrandingWebclient',
-	init (appData) {
-		settings.init(appData)
-	},
-	getAdminSystemTabs () {
-		return [
-			{
-				name: 'branding',
-				title: 'BRANDINGWEBCLIENT.ADMIN_SETTINGS_TAB_LABEL',
-				component () {
-					return import('src/../../../BrandingWebclient/vue/components/BrandingAdminSettings')
-				},
-			},
-		]
-	},
+  name: 'BrandingWebclient',
+  init (appData) {
+    settings.init(appData)
+  },
+  getAdminSystemTabs () {
+    return [
+      {
+        name: 'branding',
+        title: 'BRANDINGWEBCLIENT.ADMIN_SETTINGS_TAB_LABEL',
+        component () {
+          return import('src/../../../BrandingWebclient/vue/components/BrandingAdminSettings')
+        },
+      },
+    ]
+  },
 }
