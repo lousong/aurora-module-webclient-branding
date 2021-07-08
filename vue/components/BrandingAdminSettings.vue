f2bb7a89227a0f685e0eff3f69139955e50c691f
Evgenii <shestakovevgeniy11@gmail.com>
2021-07-08 10:54:46 +0300

    Fixed unputs

--- vue/components/BrandingAdminSettings.vue
+++ vue/components/BrandingAdminSettings.vue
@@ -9,13 +9,13 @@
           <div class="row q-mb-md">
             <div class="col-2 q-mt-sm" v-t="'BRANDINGWEBCLIENT.LOGIN_LOGO_URL_LABEL'"></div>
             <div class="col-5">
-              <q-input outlined dense class="bg-white" v-model="loginLogoUrl"/>
+              <q-input outlined dense class="bg-white input" v-model="loginLogoUrl"/>
             </div>
           </div>
           <div class="row q-mb-md">
             <div class="col-2 q-mt-sm" v-t="'BRANDINGWEBCLIENT.TABSBAR_LOGO_URL_LABEL'"></div>
             <div class="col-5">
-              <q-input outlined dense class="bg-white" v-model="tabsBarLogoUrl"/>
+              <q-input outlined dense class="bg-white input" v-model="tabsBarLogoUrl"/>
             </div>
           </div>
         </q-card-section>
@@ -111,5 +111,7 @@ export default {
 </script>
 
 <style scoped>
-
+.input {
+  border-radius: 6px;
+}
 </style>
