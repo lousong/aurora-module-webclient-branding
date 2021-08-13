df9292698406099145feed4ae69a1fe9a341e6b7
nadine-shi <nadinshi@gmail.com>
2021-08-13 17:13:49 +0300

    Add linear progress and remove progress text from buttons

--- vue/components/BrandingAdminSettings.vue
+++ vue/components/BrandingAdminSettings.vue
@@ -22,10 +22,13 @@
       </q-card>
       <div class="q-pt-md text-right">
         <q-btn unelevated no-caps dense class="q-px-sm" :ripple="false" color="primary"
-               :label="saving ? $t('COREWEBCLIENT.ACTION_SAVE_IN_PROGRESS') : $t('COREWEBCLIENT.ACTION_SAVE')"
+               :label="$t('COREWEBCLIENT.ACTION_SAVE')"
                @click="saveBrandingSettings"/>
       </div>
     </div>
+    <q-inner-loading style="justify-content: flex-start;" :showing="saving">
+      <q-linear-progress query />
+    </q-inner-loading>
   </q-scroll-area>
 </template>
 
